526452aa222204ea153668ea3987c24fd555c028
florewa <vladkoip13@gmail.com>
2025-12-10 01:06:33 +0400

    Core mechanics finished. Level design in progress.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,55 +1,71 @@
 extends Node2D
 
+# --- НАСТРОЙКИ УРОВНЯ ---
+# Эта переменная появится в Инспекторе справа.
+# В неё мы будем перетаскивать файл следующего уровня.
+@export_file("*.tscn") var next_level_path: String
+
+# --- ПЕРЕМЕННЫЕ ИГРЫ ---
 var total_coins = 0
 var collected_coins = 0
-var is_door_open = false # Флаг: открыта ли дверь?
+var is_door_open = false
 
-# Ссылки на объекты в сцене
-# Мы ищем узлы по их именам. Убедись, что в сцене они называются так же!
+# Ссылки на узлы (проверь, чтобы имена совпадали с твоей сценой!)
 @onready var score_label = $CanvasLayer/ScoreLabel
 @onready var door_sprite = $Door/Sprite2D 
-# ^ Если ты переименовывал дверь в сцене Main, поправь имя здесь (например $Door2 или просто $Door)
 
 func _ready():
-	# 1. Считаем, сколько всего монеток на уровне
+	# Считаем монеты
 	var coins_group = get_tree().get_nodes_in_group("coins")
 	total_coins = coins_group.size()
 	
-	# 2. Подключаем каждую монетку к нашей функции
+	# Подключаем сигналы монет
 	for coin in coins_group:
-		# Когда монетка испускает сигнал "collected", мы запускаем функцию _on_coin_collected
-		# (Но этот сигнал нам надо создать в монетке, см. Шаг 5)
 		coin.coin_collected.connect(_on_coin_collected)
+		
+	# Если монет на уровне 0 (например, обучающий уровень без монет), сразу открываем дверь
+	if total_coins == 0:
+		open_door()
 	
 	update_ui()
 
-# Эта функция сработает, когда подберем монету
+# Когда собрали монету
 func _on_coin_collected():
 	collected_coins += 1
 	update_ui()
 	
-	# Проверка победы
 	if collected_coins == total_coins:
 		open_door()
-		
 
-func update_ui():
-	score_label.text = "Сокровища: " + str(collected_coins) + " / " + str(total_coins)
-
+# Открытие двери (визуал)
 func open_door():
 	is_door_open = true
-	print("Дверь открыта! Беги к выходу!")
-	# Меняем картинку двери на открытую
-	# Важно: убедись, что картинка door_open.png есть в папке и загрузи ее
+	# Убедись, что путь к картинке правильный
 	door_sprite.texture = load("res://sprites/door_open.png")
-	
+	print("Дверь открыта!")
 
+# Обновление текста
+func update_ui():
+	# Если монет нет вообще, пишем "Беги к выходу"
+	if total_coins == 0:
+		score_label.text = "Найди выход!"
+	else:
+		score_label.text = "Сокровища: " + str(collected_coins) + " / " + str(total_coins)
+
+# --- ЛОГИКА ПЕРЕХОДА ---
+# Этот сигнал должен быть подключен от узла Door к этому скрипту
 func _on_door_body_entered(body):
-	# Если вошел Игрок И дверь открыта
 	if body.is_in_group("player") and is_door_open:
-		print("ПОБЕДА!")
-		# Возвращаемся в главное меню (или на экран победы)
-		call_deferred("change_scene")
-		
-func change_scene():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+		print("Уровень пройден!")
+		# Вызываем переход с небольшой задержкой (чтобы не было багов физики)
+		call_deferred("change_level")
+
+func change_level():
+	# Проверяем, указан ли следующий уровень
+	if next_level_path != "":
+		# Если указан -> грузим его
+		get_tree().change_scene_to_file(next_level_path)
+	else:
+		# Если поле пустое (например, это 5-й уровень) -> идем в Главное Меню
+		# Убедись, что путь к меню правильный!
+		get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
