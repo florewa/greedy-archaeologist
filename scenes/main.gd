853fad529eb00221915d8879ac501cd6e27e75c0
florewa <vladkoip13@gmail.com>
2025-12-09 21:15:38 +0400

    Stage 7: Главное меню и завершение цикла игры

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var total_coins = 0
 var collected_coins = 0
+var is_door_open = false # Флаг: открыта ли дверь?
 
 # Ссылки на объекты в сцене
 # Мы ищем узлы по их именам. Убедись, что в сцене они называются так же!
@@ -30,12 +31,25 @@ func _on_coin_collected():
 	# Проверка победы
 	if collected_coins == total_coins:
 		open_door()
+		
 
 func update_ui():
 	score_label.text = "Сокровища: " + str(collected_coins) + " / " + str(total_coins)
 
 func open_door():
-	print("Дверь открыта!")
+	is_door_open = true
+	print("Дверь открыта! Беги к выходу!")
 	# Меняем картинку двери на открытую
 	# Важно: убедись, что картинка door_open.png есть в папке и загрузи ее
 	door_sprite.texture = load("res://sprites/door_open.png")
+	
+
+func _on_door_body_entered(body):
+	# Если вошел Игрок И дверь открыта
+	if body.is_in_group("player") and is_door_open:
+		print("ПОБЕДА!")
+		# Возвращаемся в главное меню (или на экран победы)
+		call_deferred("change_scene")
+		
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
